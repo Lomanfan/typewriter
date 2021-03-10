0dca5fa6c942cb3921180b051ba77964f9ab3e15
Liuming <lfan.canada@gmail.com>
2021-03-10 13:56:06 +0000

    typewriter - Animated Sentence

---
+++ typewriter.js
@@ -0,0 +1,33 @@
+
+// const senArray = sentence.split(" ");
+const sentence = "hello there from lighthouse labs";
+
+
+let timeOut = 0;
+for (const char of sentence) { 
+  // console.log(char);
+  setTimeout(() => {
+    process.stdout.write(char);   
+    
+    }, timeOut); // <= set a time increment 
+    timeOut += 80;
+    
+  };
+  
+  setTimeout (() => {    // after sentence is printed, add \n to stdout. 
+
+  process.stdout.write("\n"); 
+  }, timeOut);
+  
+
+
+
+
+
+
+
+
+
+
+  
+  
